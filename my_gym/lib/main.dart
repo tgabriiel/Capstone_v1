6d0a06063388cadd7814874a5ed156d29fb6fd5e
Gabriel T <98978046+gtirado218@users.noreply.github.com>
2023-11-25 10:27:13 -0800

    Updated Final Gym App Code
    This is the final updated Gym app code using flutter. This works and may have additional smaller updates to add some fixes and incremental functions.

--- my_gym/lib/main.dart
+++ my_gym/lib/main.dart
@@ -1,4 +1,18 @@
+/*
+Name: Gabriel Tirado
+Class: Capstone 499 - Computer Science
+Project: Gym App Project using Flutter
+Date: Due December 1, 2023
+*/
+
+/*
+import 'dart:js_util';
+import 'package:flutter/services.dart';
+import 'dart:developer';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/foundation.dart';
+*/
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:provider/provider.dart';
@@ -33,42 +47,28 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-
-  var favorites = <WordPair>[];
-
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
-    } else {
-      favorites.add(current);
-    }
-    notifyListeners();
-  }
-}
+class MyAppState extends ChangeNotifier {}
 
 class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class SharedPreferences {
+  static getInstance() {}
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   var selectedIndex = 0;
+  List<String> daily = [];
 
   @override
   Widget build(BuildContext context) {
     final List<Widget> page = [
       WorkoutTracking(),
-      MealTracking(),
+      CurrentWorkout(),
       CalorieTracking(),
     ];
-
     return LayoutBuilder(builder: (context, constraints) {
       return Scaffold(
           body: page[selectedIndex],
@@ -86,15 +86,15 @@ class _MyHomePageState extends State<MyHomePage> {
                 tabs: [
                   GButton(
                     icon: Icons.fitness_center,
-                    text: 'Workout Tracking',
+                    text: 'Workout tracking',
                   ),
                   GButton(
                     icon: Icons.food_bank,
-                    text: 'Meal Tracking',
+                    text: 'Current workout',
                   ),
                   GButton(
                     icon: Icons.incomplete_circle,
-                    text: 'Calorie Tracking',
+                    text: 'Calorie tracking',
                   )
                 ],
                 selectedIndex: selectedIndex,
@@ -110,135 +110,200 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+//This is where you select the workouts you want to keep track of
 class WorkoutTracking extends StatelessWidget {
+  final List<String> workouts = [
+    'Deadlift',
+    'Pull up',
+    'Seated row',
+    'Squats',
+    'Bench Press',
+    'Lunge',
+    'Curls'
+  ];
+  final List<int> workoutTimes = <int>[];
+
+  /*void addItemToList() {
+    setState(() {
+      names.insert(0, nameController.text);
+      msgCount.insert(0, 0);
+    });
+  }*/
+
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () async {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ],
+    const title = "Workouts";
+    return MaterialApp(
+        title: 'Workouts',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(title),
           ),
-        ],
+          body: ListView.builder(
+              itemCount: workouts.length,
+              prototypeItem: ListTile(
+                title: Text(workouts.first),
+              ),
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(workouts[index]),
+                  tileColor: Color.fromARGB(255, 222, 228, 227),
+                  trailing: TextButton(
+                    onPressed: () {},
+                    child: Text('Add to current workout'),
+                  ),
+                );
+              }),
+        ));
+  }
+}
+
+//This is where the calorie tracking portion of the program begins
+class CurrentWorkout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("Current workout"),
       ),
     );
   }
 }
 
-class SharedPreferences {
-  static getInstance() {}
-}
-
-class MealTracking extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('You have added no meals yet...\n'
-            'You should add some meals to get started! '),
-      );
-    }
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have ' '${appState.favorites.length} favorites:'),
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
-          ),
-      ],
-    );
-  }
-}
-
+//This is where the calorie tracking portion of the program begins
 class CalorieTracking extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('You have not added any calorie input yet...\n'
-            'Start adding now!'),
-      );
-    }
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
-          ),
-      ],
-    );
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
+  int totalCal = 0;
 
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-    var style = theme.textTheme.displayMedium!
-        .copyWith(color: theme.colorScheme.onPrimary);
-
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(
-          pair.asLowerCase,
-          style: style,
-          semanticsLabel: pair.asPascalCase,
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          foregroundColor: Color.fromARGB(255, 43, 43, 46),
+          backgroundColor: Color.fromARGB(255, 162, 105, 226),
+          title: Text('Calorie Tracker'),
         ),
-      ),
-    );
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(0),
+              child: ButtonBar(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  FilledButton(
+                    onPressed: () {
+                      totalCal = totalCal + 10;
+                      print(totalCal);
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text(
+                              "10 Calories added. $totalCal Calories today."),
+                          duration: Duration(seconds: 0, milliseconds: 500)));
+                    },
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Color.fromARGB(255, 57, 116, 86))),
+                    child: Text('Add 10 Calories'),
+                  ),
+                  FilledButton(
+                    onPressed: () {
+                      totalCal = totalCal + 25;
+                      print(totalCal);
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text('25 Calories added. ' +
+                              totalCal.toString() +
+                              " Calories today."),
+                          duration: Duration(seconds: 0, milliseconds: 500)));
+                    },
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Color.fromARGB(255, 57, 116, 86))),
+                    child: Text('Add 25 Calories'),
+                  ),
+                  FilledButton(
+                    onPressed: () {
+                      totalCal = totalCal + 50;
+                      print(totalCal);
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text("50 Calories added. $totalCal today."),
+                          duration: Duration(seconds: 0, milliseconds: 500)));
+                    },
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Color.fromARGB(255, 57, 116, 86))),
+                    child: Text('Add 50 Calories'),
+                  ),
+                  FilledButton(
+                    onPressed: () {
+                      totalCal = totalCal + 100;
+                      print(totalCal);
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text("100 Calories added. $totalCal today."),
+                          duration: Duration(seconds: 0, milliseconds: 500)));
+                    },
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Color.fromARGB(255, 57, 116, 86))),
+                    child: Text('Add 100 Calories'),
+                  ),
+                  FilledButton(
+                    onPressed: () {
+                      totalCal = totalCal + 250;
+                      print(totalCal);
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text("250 Calories added. $totalCal today."),
+                          duration: Duration(seconds: 0, milliseconds: 500)));
+                    },
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Color.fromARGB(255, 57, 116, 86))),
+                    child: Text('Add 250 Calories'),
+                  ),
+                  FilledButton(
+                    onPressed: () {
+                      totalCal = totalCal + 500;
+                      print(totalCal);
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text("500 Calories added. $totalCal today."),
+                          duration: Duration(seconds: 0, milliseconds: 500)));
+                    },
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Color.fromARGB(255, 57, 116, 86))),
+                    child: Text('Add 500 Calories'),
+                  ),
+                  FilledButton(
+                    onPressed: () {
+                      totalCal = totalCal + 1000;
+                      print(totalCal);
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text('1,000 Calories added. ' +
+                              totalCal.toString() +
+                              " Calories today."),
+                          duration: Duration(seconds: 0, milliseconds: 500)));
+                    },
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Color.fromARGB(255, 57, 116, 86))),
+                    child: Text('Add 1000 Calories'),
+                  ),
+                  FilledButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text(totalCal.toString() + " Calories today."),
+                        duration: Duration(seconds: 0, milliseconds: 500),
+                      ));
+                    },
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Color.fromARGB(255, 57, 116, 86))),
+                    child: Text("Click here to print total calories"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
